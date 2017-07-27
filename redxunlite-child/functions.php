456156943696aff95705ed87e12a84cff5255576
raoasifraza11 <raoasifraza1@hotmail.com>
2017-07-27 16:24:10 +0500

    SEO Meta description
    Improve SEO, Generate Login page, Fix title tab

--- redxunlite-child/functions.php
+++ redxunlite-child/functions.php
@@ -9,8 +9,13 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
 }
+
+
+function my_custom_login() {
+	echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/login/login-styles.css" />';
+}
+add_action('login_head', 'my_custom_login');
 
 // Custom Function to Include
 function meta_seo_links() {
@@ -18,4 +23,3 @@ function meta_seo_links() {
     echo '<meta name="keywords" content="coding,programming websites,online programs,code for free">' . "\n";
 }
 add_action( 'wp_head', 'meta_seo_links' );
-
