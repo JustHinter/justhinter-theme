c9928bf906e601ed4720e0f1c34e612c6369c9b1
raoasifraza11 <raoasifraza1@hotmail.com>
2017-07-28 01:08:43 +0500

    Howdy replece with selected str

--- redxunlite-child/functions.php
+++ redxunlite-child/functions.php
@@ -59,3 +59,16 @@ function logout_page() {
     exit;
 }
 add_action('wp_logout','logout_page');
+
+
+/** Howdy replace with '' */
+function replace_howdy($wp_admin_bar) {
+    $my_account=$wp_admin_bar->get_node('my-account');
+    $newtitle = str_replace('Howdy,', '', $my_account->title );
+    $wp_admin_bar->add_node(array(
+            'id' => 'my-account',
+            'title' => $newtitle,
+        )
+    );
+}
+add_filter('admin_bar_menu', 'replace_howdy',25);
