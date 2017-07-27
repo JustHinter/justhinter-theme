1328837acbd4ca46d71ca06e30d4148a63ced852
raoasifraza11 <raoasifraza1@hotmail.com>
2017-07-27 13:31:31 +0500

    Update meta tags

--- redxunlite-child/functions.php
+++ redxunlite-child/functions.php
@@ -11,3 +11,11 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+// Custom Function to Include
+function meta_seo_links() {
+    echo '<meta name="description" content="Just hinter is the is the collection of techniques, skills, methods." >' . "\n";
+    echo '<meta name="keywords" content="coding,programming websites,online programs,code for free">' . "\n";
+}
+add_action( 'wp_head', 'meta_seo_links' );
+
